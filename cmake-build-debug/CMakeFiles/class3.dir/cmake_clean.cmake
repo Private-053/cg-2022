1a31d62a6bf3c87ac4b33ddfe53922681c69d9ea
Private-053 <hugojbrandao@gmail.com>
2022-03-07 23:51:47 +0000

    plane

---
+++ cmake-build-debug/CMakeFiles/class3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/class3.dir/generator.cpp.obj"
+  "CMakeFiles/class3.dir/generator.cpp.obj.d"
+  "class3.exe"
+  "class3.exe.manifest"
+  "class3.pdb"
+  "libclass3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/class3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ generator.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cmath>
+
+struct Point{
+    float x;
+    float y;
+    float z;
+};
+
+void write_point(Point p, std::ofstream &filename){
+    filename << std::to_string(p.x) + "," +
+                std::to_string(p.y) + "," +
+                std::to_string(p.z) + ";";
+}
+
+void make_plane(int size, int divs, std::string filename){
+    std::ofstream outfile(filename, std::ios::binary | std::ios::out);
+
+    int num_points = divs*divs;
+    int intervalo = size/divs;
+    Point p;
+    for(int i = 0; i<size;){
+        for(int j = 0; j<size;){
+            p.x = i;
+            p.y = 0;
+            p.z = j;
+            write_point(p,outfile);
+
+            p.x = i;
+            p.y = 0;
+            p.z = j + intervalo;
+            write_point(p,outfile);
+
+            p.x = i + intervalo;
+            p.y = 0;
+            p.z = j;
+            write_point(p,outfile);
+
+            j +=intervalo;
+        }
+
+        for(int k = intervalo; k<=size;){
+            p.x = i;
+            p.y = 0;
+            p.z = k;
+            write_point(p,outfile);
+
+            p.x = i + intervalo;
+            p.y = 0;
+            p.z = k - intervalo;
+            write_point(p,outfile);
+
+            p.x = i + intervalo;
+            p.y = 0;
+            p.z = k;
+            write_point(p,outfile);
+
+            k += intervalo;
+
+        }
+        i+=intervalo;
+    }
+
+}
+
+
+
+int main(int argc, char** argv){
+    if(argc<=2){
+        std::cout << "Not enough arguments" << std::endl;
+        return 1;
+    }
+
+    std::string figura = argv[1];
+
+    if(figura.compare("sphere")){
+
+    }
+    if(figura.compare("box")){
+
+    }
+    if(figura.compare("cone")) {
+    }
+
+    if(figura.compare("plane")) {
+        make_plane(*argv[2],*argv[3],argv[4]);
+    }
+
+}
